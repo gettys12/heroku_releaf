35d186638e74c64a028e3440b69c3ab6170f9064
gettys12 <katemary.murphy@gmail.com>
2014-02-02 16:50:30 -0500

    required sass rails in app.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 # If you precompile assets before deploying to production, use this line
 require 'rails/all'
-
+require 'sass-rails'
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
