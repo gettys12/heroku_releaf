d7cb311f4fcdd211509a116211c3843ad53d1505
gettys12 <katemary.murphy@gmail.com>
2014-02-04 13:42:56 -0500

    changes to resource model to validate the content type of the image to be uploaded

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,6 +4,7 @@ class Resource < ActiveRecord::Base
   belongs_to :library
   has_attached_file :image
 
-  
-  
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+
+
 end
