4d9d736be3169e3ed38c54d0df8d82e7508288fc
gettys12 <katemary.murphy@gmail.com>
2014-02-17 09:40:06 -0500

    changed production config line to enable asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,7 @@ Releaf::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
   # Generate digests for assets URLs.
   config.assets.digest = true
 
