d0530c58ffb4e478979cfb021f1133b238c9bfc4
gettys12 <katemary.murphy@gmail.com>
2014-02-17 08:19:32 -0500

    deleted s3.yml and changed config options in environment files

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,14 +2,8 @@ class Resource < ActiveRecord::Base
  
   attr_accessible :type, :library_id, :image
   belongs_to :library
-  Paperclip.options[:command_path] = 'C:\RailsInstaller\ImageMagick'
   has_attached_file :image,
-                    :styles => { :medium => "x300", :thumb => "x100" },
-                    :storage => :s3,
-                    :bucket => 'releaf-files',
-                    :s3_credentials => "#{Rails.root}/config/s3.yml",
-                    :path => ":attachment/:id/:style.:extension",
-                    :bucket => "releaf-files"
+                    :styles => { :medium => "x300", :thumb => "x100" }
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
 
 
